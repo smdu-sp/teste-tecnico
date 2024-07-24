6b06390d2d2705e12e3b819d4710a98279e1f2aa
Caue Souza <caue.souzapl@gmail.com>
2024-07-24 18:36:27 -0300

    finalizado

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const port = 3000;
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
   console.log(`Aplicação rodando em http://localhost:${port}`)
 }
